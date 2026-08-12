497f9ef683162f8df6b3bb8a5b7d889946d632bc
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:47:45 +0300

    Publish Blazor standalone project file: OldMainLayout.razor.cs

---
+++ client/Shared/OldMainLayout.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Radzen;
+using Radzen.Blazor;
+
+namespace Cpdhelpdesk.Layouts
+{
+    public partial class OldMainLayoutComponent
+    {
+
+    }
+}
