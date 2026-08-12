0275a2b3f4c868cbf629643daf5a2a260c02055c
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:37:37 +0300

    Publish Blazor standalone project file: EditSoftwareModulescatlist.razor.designer.cs

---
+++ client/Pages/EditSoftwareModulescatlist.razor.designer.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using Cpdhelpdesk.Models.Authenticationconn;
+using Cpdhelpdesk.Models;
+using Cpdhelpdesk.Client.Pages;
+
+namespace Cpdhelpdesk.Pages
+{
+    public partial class EditSoftwareModulescatlistComponent : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, dynamic> Attributes { get; set; }
+
+        public void Reload()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+        }
+
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+
+        [Inject]
+        protected AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        [Inject]
+        protected AuthenticationconnService Authenticationconn { get; set; }
+
+        [Parameter]
+        public dynamic sprModulecatid { get; set; }
+
+        bool _hasChanges;
+        protected bool hasChanges
+        {
+            get
+            {
+                return _hasChanges;
+            }
+            set
+            {
+                if (!object.Equals(_hasChanges, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "hasChanges", NewValue = value, OldValue = _hasChanges };
+                    _hasChanges = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        bool _canEdit;
+        protected bool canEdit
+        {
+            get
+            {
+                return _canEdit;
+            }
+            set
+            {
+                if (!object.Equals(_canEdit, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "canEdit", NewValue = value, OldValue = _canEdit };
+                    _canEdit = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        Cpdhelpdesk.Models.Authenticationconn.SoftwareModulescatlist _softwaremodulescatlist;
+        protected Cpdhelpdesk.Models.Authenticationconn.SoftwareModulescatlist softwaremodulescatlist
+        {
+            get
+            {
+                return _softwaremodulescatlist;
+            }
+            set
+            {
+                if (!object.Equals(_softwaremodulescatlist, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "softwaremodulescatlist", NewValue = value, OldValue = _softwaremodulescatlist };
+                    _softwaremodulescatlist = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            await Security.InitializeAsync(AuthenticationStateProvider);
+            if (!Security.IsAuthenticated())
+            {
+                UriHelper.NavigateTo("Login", true);
+            }
+            else
+            {
+                await Load();
+            }
+        }
+        protected async System.Threading.Tasks.Task Load()
+        {
+            hasChanges = false;
+
+            canEdit = true;
+
+            var authenticationconnGetSoftwareModulescatlistBysprModulecatidResult = await Authenticationconn.GetSoftwareModulescatlistBysprModulecatid(sprModulecatid:sprModulecatid);
+            softwaremodulescatlist = authenticationconnGetSoftwareModulescatlistBysprModulecatidResult;
+
+            canEdit = authenticationconnGetSoftwareModulescatlistBysprModulecatidResult != null;
+        }
+
+        protected async System.Threading.Tasks.Task CloseButtonClick(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+
+        protected async System.Threading.Tasks.Task Button0Click(MouseEventArgs args)
+        {
+            await this.Load();
+        }
+
+        protected async System.Threading.Tasks.Task Form0Submit(Cpdhelpdesk.Models.Authenticationconn.SoftwareModulescatlist args)
+        {
+            try
+            {
+                var authenticationconnUpdateSoftwareModulescatlistResult = await Authenticationconn.UpdateSoftwareModulescatlist(sprModulecatid:sprModulecatid, softwareModulescatlist:softwaremodulescatlist);
+                if (authenticationconnUpdateSoftwareModulescatlistResult.StatusCode != System.Net.HttpStatusCode.PreconditionFailed) {
+                  DialogService.Close(softwaremodulescatlist);
+                }
+
+                hasChanges = authenticationconnUpdateSoftwareModulescatlistResult.StatusCode == System.Net.HttpStatusCode.PreconditionFailed;
+            }
+            catch (System.Exception authenticationconnUpdateSoftwareModulescatlistException)
+            {
+                NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to update SoftwareModulescatlist" });
+
+            hasChanges = authenticationconnUpdateSoftwareModulescatlistException.Message.Contains("412");
+
+            if (!authenticationconnUpdateSoftwareModulescatlistException.Message.Contains("412")) {
+                canEdit = false;
+            }
+            }
+        }
+
+        protected async System.Threading.Tasks.Task Button4Click(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+    }
+}
