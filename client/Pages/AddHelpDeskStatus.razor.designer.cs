d237630d0119f31c4061f6573b759c28ba6f17df
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:32:25 +0300

    Publish Blazor standalone project file: AddHelpDeskStatus.razor.designer.cs

---
+++ client/Pages/AddHelpDeskStatus.razor.designer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using Cpdhelpdesk.Models.Authenticationconn;
+using Cpdhelpdesk.Models;
+using Cpdhelpdesk.Client.Pages;
+
+namespace Cpdhelpdesk.Pages
+{
+    public partial class AddHelpDeskStatusComponent : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, dynamic> Attributes { get; set; }
+
+        public void Reload()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+        }
+
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+
+        [Inject]
+        protected AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        [Inject]
+        protected AuthenticationconnService Authenticationconn { get; set; }
+
+        Cpdhelpdesk.Models.Authenticationconn.HelpDeskStatus _helpdeskstatus;
+        protected Cpdhelpdesk.Models.Authenticationconn.HelpDeskStatus helpdeskstatus
+        {
+            get
+            {
+                return _helpdeskstatus;
+            }
+            set
+            {
+                if (!object.Equals(_helpdeskstatus, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "helpdeskstatus", NewValue = value, OldValue = _helpdeskstatus };
+                    _helpdeskstatus = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            await Security.InitializeAsync(AuthenticationStateProvider);
+            if (!Security.IsAuthenticated())
+            {
+                UriHelper.NavigateTo("Login", true);
+            }
+            else
+            {
+                await Load();
+            }
+        }
+        protected async System.Threading.Tasks.Task Load()
+        {
+            helpdeskstatus = new Cpdhelpdesk.Models.Authenticationconn.HelpDeskStatus(){};
+        }
+
+        protected async System.Threading.Tasks.Task Form0Submit(Cpdhelpdesk.Models.Authenticationconn.HelpDeskStatus args)
+        {
+            try
+            {
+                var authenticationconnCreateHelpDeskStatusResult = await Authenticationconn.CreateHelpDeskStatus(helpDeskStatus:helpdeskstatus);
+                DialogService.Close(helpdeskstatus);
+            }
+            catch (System.Exception authenticationconnCreateHelpDeskStatusException)
+            {
+                NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to create new HelpDeskStatus!" });
+            }
+        }
+
+        protected async System.Threading.Tasks.Task Button2Click(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+    }
+}
