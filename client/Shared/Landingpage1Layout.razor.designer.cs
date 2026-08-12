f8bc8d559a60cd9ada5d6b2f8ffa8cb466a5aec2
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:47:15 +0300

    Publish Blazor standalone project file: Landingpage1Layout.razor.designer.cs

---
+++ client/Shared/Landingpage1Layout.razor.designer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using Cpdhelpdesk.Models.Authenticationconn;
+using Cpdhelpdesk.Models;
+using Microsoft.JSInterop;
+using Cpdhelpdesk.Pages;
+
+namespace Cpdhelpdesk.Layouts
+{
+    public partial class Landingpage1LayoutComponent : LayoutComponentBase
+    {
+        [Inject]
+        protected Microsoft.JSInterop.IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+
+        [Inject]
+        protected AuthenticationconnService Authenticationconn { get; set; }
+
+        protected RadzenBody body0;
+
+        private void Authenticated()
+        {
+             StateHasChanged();
+        }
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+             if (Security != null)
+             {
+                  Security.Authenticated += Authenticated;
+
+                  await Security.InitializeAsync(AuthenticationStateProvider);
+             }
+        }
+    }
+}
