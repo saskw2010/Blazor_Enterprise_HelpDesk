3957c2dac636fb8432161eef819a8c3adcc67639
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:39:19 +0300

    Publish Blazor standalone project file: Icpdhelpdesk.razor.cs

---
+++ client/Pages/Icpdhelpdesk.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Radzen;
+using Radzen.Blazor;
+using Microsoft.AspNetCore.Components;
+using Cpdhelpdesk.Client.Pages;
+
+namespace Cpdhelpdesk.Pages
+{
+    public partial class IcpdhelpdeskComponent
+    {
+        [Inject]
+        protected Microsoft.Extensions.Localization.IStringLocalizer<Icpdhelpdesk> Loc { get; set; }
+    }
+}
