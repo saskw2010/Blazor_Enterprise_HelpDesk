599c649cb308726f5bf72ecef9b5e29ce5cc5744
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:32:47 +0300

    Publish Blazor standalone project file: AddHelpDeskTicketDetailmos.razor.cs

---
+++ client/Pages/AddHelpDeskTicketDetailmos.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Radzen;
+using Radzen.Blazor;
+using Microsoft.AspNetCore.Components;
+
+namespace Cpdhelpdesk.Pages
+{
+    public partial class AddHelpDeskTicketDetailComponent
+    {
+         
+    }
+}
