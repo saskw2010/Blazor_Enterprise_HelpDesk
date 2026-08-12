7173ff25b8b0593e57134c6151e5fcd17cb1651d
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:54:43 +0300

    Publish Blazor standalone project file: HelpDeskEmail.cs

---
+++ server/Models/MossMoles/HelpDeskEmail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cpdhelpdesk.Shared
+{
+    public class HelpDeskEmail
+    {
+        public long id { get; set; }
+        public string EmailType { get; set; }
+        public string EmailAddress { get; set; }
+        public string EmailAddresscc { get; set; }
+
+        public string EmailAddresasinto { get; set; }
+        public string TicketGuid { get; set; }
+    }
+}
