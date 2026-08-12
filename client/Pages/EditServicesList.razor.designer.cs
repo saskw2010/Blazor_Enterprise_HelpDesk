c3c09714ac56ea0f332ee3d7ce1c564ccebefc87
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:37:22 +0300

    Publish Blazor standalone project file: EditServicesList.razor.designer.cs

---
+++ client/Pages/EditServicesList.razor.designer.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using Cpdhelpdesk.Models.Authenticationconn;
+using Cpdhelpdesk.Models;
+using Cpdhelpdesk.Client.Pages;
+
+namespace Cpdhelpdesk.Pages
+{
+    public partial class EditServicesListComponent : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, dynamic> Attributes { get; set; }
+
+        public void Reload()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+        }
+
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+
+        [Inject]
+        protected AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        [Inject]
+        protected AuthenticationconnService Authenticationconn { get; set; }
+
+        [Parameter]
+        public dynamic ServiceID { get; set; }
+
+        bool _hasChanges;
+        protected bool hasChanges
+        {
+            get
+            {
+                return _hasChanges;
+            }
+            set
+            {
+                if (!object.Equals(_hasChanges, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "hasChanges", NewValue = value, OldValue = _hasChanges };
+                    _hasChanges = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        bool _canEdit;
+        protected bool canEdit
+        {
+            get
+            {
+                return _canEdit;
+            }
+            set
+            {
+                if (!object.Equals(_canEdit, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "canEdit", NewValue = value, OldValue = _canEdit };
+                    _canEdit = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        Cpdhelpdesk.Models.Authenticationconn.ServicesList _serviceslist;
+        protected Cpdhelpdesk.Models.Authenticationconn.ServicesList serviceslist
+        {
+            get
+            {
+                return _serviceslist;
+            }
+            set
+            {
+                if (!object.Equals(_serviceslist, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "serviceslist", NewValue = value, OldValue = _serviceslist };
+                    _serviceslist = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        Cpdhelpdesk.Models.Authenticationconn.ServiceCatglist _getByServiceCatglistsForServiceCatgIDResult;
+        protected Cpdhelpdesk.Models.Authenticationconn.ServiceCatglist getByServiceCatglistsForServiceCatgIDResult
+        {
+            get
+            {
+                return _getByServiceCatglistsForServiceCatgIDResult;
+            }
+            set
+            {
+                if (!object.Equals(_getByServiceCatglistsForServiceCatgIDResult, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "getByServiceCatglistsForServiceCatgIDResult", NewValue = value, OldValue = _getByServiceCatglistsForServiceCatgIDResult };
+                    _getByServiceCatglistsForServiceCatgIDResult = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        IEnumerable<Cpdhelpdesk.Models.Authenticationconn.ServiceCatglist> _getServiceCatglistsForServiceCatgIDResult;
+        protected IEnumerable<Cpdhelpdesk.Models.Authenticationconn.ServiceCatglist> getServiceCatglistsForServiceCatgIDResult
+        {
+            get
+            {
+                return _getServiceCatglistsForServiceCatgIDResult;
+            }
+            set
+            {
+                if (!object.Equals(_getServiceCatglistsForServiceCatgIDResult, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "getServiceCatglistsForServiceCatgIDResult", NewValue = value, OldValue = _getServiceCatglistsForServiceCatgIDResult };
+                    _getServiceCatglistsForServiceCatgIDResult = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        int _getServiceCatglistsForServiceCatgIDCount;
+        protected int getServiceCatglistsForServiceCatgIDCount
+        {
+            get
+            {
+                return _getServiceCatglistsForServiceCatgIDCount;
+            }
+            set
+            {
+                if (!object.Equals(_getServiceCatglistsForServiceCatgIDCount, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "getServiceCatglistsForServiceCatgIDCount", NewValue = value, OldValue = _getServiceCatglistsForServiceCatgIDCount };
+                    _getServiceCatglistsForServiceCatgIDCount = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            await Security.InitializeAsync(AuthenticationStateProvider);
+            if (!Security.IsAuthenticated())
+            {
+                UriHelper.NavigateTo("Login", true);
+            }
+            else
+            {
+                await Load();
+            }
+        }
+        protected async System.Threading.Tasks.Task Load()
+        {
+            hasChanges = false;
+
+            canEdit = true;
+
+            var authenticationconnGetServicesListByServiceIdResult = await Authenticationconn.GetServicesListByServiceId(serviceId:ServiceID);
+            serviceslist = authenticationconnGetServicesListByServiceIdResult;
+
+            canEdit = authenticationconnGetServicesListByServiceIdResult != null;
+
+            if (this.serviceslist.ServiceCatgID != null)
+            {
+                var authenticationconnGetServiceCatglistByServiceCatgIdResult = await Authenticationconn.GetServiceCatglistByServiceCatgId(serviceCatgId:this.serviceslist.ServiceCatgID);
+                getByServiceCatglistsForServiceCatgIDResult = authenticationconnGetServiceCatglistByServiceCatgIdResult;
+            }
+        }
+
+        protected async System.Threading.Tasks.Task CloseButtonClick(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+
+        protected async System.Threading.Tasks.Task Button0Click(MouseEventArgs args)
+        {
+            await this.Load();
+        }
+
+        protected async System.Threading.Tasks.Task Form0Submit(Cpdhelpdesk.Models.Authenticationconn.ServicesList args)
+        {
+            try
+            {
+                var authenticationconnUpdateServicesListResult = await Authenticationconn.UpdateServicesList(serviceId:ServiceID, servicesList:serviceslist);
+                if (authenticationconnUpdateServicesListResult.StatusCode != System.Net.HttpStatusCode.PreconditionFailed) {
+                  DialogService.Close(serviceslist);
+                }
+
+                hasChanges = authenticationconnUpdateServicesListResult.StatusCode == System.Net.HttpStatusCode.PreconditionFailed;
+            }
+            catch (System.Exception authenticationconnUpdateServicesListException)
+            {
+                NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to update ServicesList" });
+
+            hasChanges = authenticationconnUpdateServicesListException.Message.Contains("412");
+
+            if (!authenticationconnUpdateServicesListException.Message.Contains("412")) {
+                canEdit = false;
+            }
+            }
+        }
+
+        protected async System.Threading.Tasks.Task ServiceCatgIdLoadData(LoadDataArgs args)
+        {
+            var authenticationconnGetServiceCatglistsResult = await Authenticationconn.GetServiceCatglists(filter:$"{args.Filter}", orderby:$"{args.OrderBy}", top:args.Top, skip:args.Skip, count:true);
+            getServiceCatglistsForServiceCatgIDResult = authenticationconnGetServiceCatglistsResult.Value.AsODataEnumerable();
+
+            getServiceCatglistsForServiceCatgIDCount = authenticationconnGetServiceCatglistsResult.Count;
+        }
+
+        protected async System.Threading.Tasks.Task Button4Click(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+    }
+}
