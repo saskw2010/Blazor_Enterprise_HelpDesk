58008e55c29cd38f1e17fdb8fe184e97664f0808
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 00:52:43 +0300

    Publish Blazor standalone project file: TokenProviderOptions.cs

---
+++ server/Authentication/TokenProviderOptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Cpdhelpdesk.Authentication
+{
+    public class TokenProviderOptions
+    {
+        public static string Audience { get; } = "CpdhelpdeskAudience";
+        public static string Issuer { get; } = "Cpdhelpdesk";
+        public static SymmetricSecurityKey Key { get; } = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("CpdhelpdeskSecretSecurityKeyCpdhelpdesk"));
+        public static TimeSpan Expiration { get; } = TimeSpan.FromMinutes(5);
+        public static SigningCredentials SigningCredentials { get; } = new SigningCredentials(Key, SecurityAlgorithms.HmacSha256);
+    }
+}
